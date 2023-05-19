840a91d31c99c897574958fe00acbc1b8cce4658
maximdudai <maxim.dudai01@gmail.com>
2023-05-19 21:15:59 +0100

    setup & remove useless file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  darkMode: ['class', '[data-theme="dark"]'],
+  theme: {
+    extend: {
+        colors: {
+            dark: '#292929',
+            light: '#dfdfdf'
+        }
+    },
+  },
+  plugins: [],
+}
+
