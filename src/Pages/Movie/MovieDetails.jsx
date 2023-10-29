436dde6cedea748ea6d32bf7aa5757f8fe6ee42a
maximdudai <maxim.dudai01@gmail.com>
2023-10-29 19:20:59 +0000

    Adaptable Movie Data page

--- src/Pages/Movie/MovieDetails.jsx
+++ src/Pages/Movie/MovieDetails.jsx
@@ -22,7 +22,10 @@ export const MovieDetails = () => {
   useEffect(() => {
     const fetchMovieFullDetails = async () => {
       const movieData = await searchMovieById(getNumbersFromString(query));
-      const movieImages = await getMovieImages(getNumbersFromString(query), "backdrop");
+      const movieImages = await getMovieImages(
+        getNumbersFromString(query),
+        "backdrop"
+      );
 
       setMovieData(movieData);
       console.log(movieData);
@@ -56,34 +59,36 @@ export const MovieDetails = () => {
       ? `https://image.tmdb.org/t/p/original/${company}`
       : `https://static-00.iconduck.com/assets.00/404-page-not-found-illustration-2048x998-yjzeuy4v.png`;
 
-
   return (
     <>
       <Navigation />
 
-      <main className="flex justify-center">
+      <main>
         {isLoading ? (
           <div className="loadingMovieData flex items-center justify-center bg-white/5 p-5 m-2 rounded">
             <span className="px-2">Loading</span>
             <span className="animate-spin px-2">
-              <AiOutlineLoading3Quarters />{" "}
+              <AiOutlineLoading3Quarters />
             </span>
           </div>
         ) : (
-          <div className="movieFullDetails p-2 flex flex-col justify-center items-center md:w-3/4">
-            <div className="movieTitle bg-white/5 p-2 rounded w-full md:w-1/2 md:text-center">
-              <p className="my-2 text-lg md:text-2xl">{movieData?.title}</p>
-            </div>
-            <div className="movieDataCard my-5">
-              <div className="movieBackdropImage md:w-full">
+          <div className="movieFullDetails p-5 my-5 flex flex-col lg:flex-row gap-2">
+            <div className="movieInformationContainer flex flex-col justify-between">
+              <div className="movieTitle p-2 my-1 bg-white/10 rounded w-full md:text-center">
+                <p className="my-2 text-lg md:text-2xl">{movieData?.title}</p>
+              </div>
+              <div className="movieBackdropImage">
                 <img
                   className="rounded"
                   src={selectedPosterImage}
                   alt="Movie Poster"
                 />
               </div>
+            </div>
+
+            <div className="movieFullDataContainer flex flex-col justify-center gap-2 w-full shadow-md shadow-black bg-black/20 p-3">
               <div className="movieReleaseDate">
-                <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400 shadow-sm shadow-black rounded">
+                <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400">
                   Release Date
                 </p>
                 <div className="movieReleaseDateList flex flex-wrap gap-2">
@@ -93,7 +98,7 @@ export const MovieDetails = () => {
                 </div>
               </div>
               <div className="movieRuntime">
-                <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400 shadow-sm shadow-black rounded">
+                <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400">
                   Runtime
                 </p>
                 <div className="movieRuntimeList flex flex-wrap gap-2">
@@ -103,7 +108,7 @@ export const MovieDetails = () => {
                 </div>
               </div>
               <div className="moviePosterImageGallery ">
-                <p className="flex justify-between items-center text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400 shadow-sm shadow-black rounded">
+                <p className="flex justify-between items-center text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400">
                   <span>Gallery</span>
                   <span className="border-[1px] border-red-500 p-2 rounded">
                     <GrGallery className="bg-white" />
@@ -123,35 +128,51 @@ export const MovieDetails = () => {
                   })}
                 </div>
               </div>
-              <div className="movieCategories">
-                <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400 shadow-sm shadow-black rounded">
-                  Categories
-                </p>
-                <div className="movieCategoriesList flex flex-wrap gap-2">
-                  {movieData?.genres?.map((genre, index) => {
-                    return (
-                      <span
-                        key={index}
-                        className="bg-white/5 p-2 rounded text-xs"
-                      >
-                        {genre?.name}
-                      </span>
-                    );
-                  })}
+              <div className="movieDataBox md:flex justify-between">
+                <div className="movieCategories">
+                  <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400">
+                    Categories
+                  </p>
+                  <div className="movieCategoriesList flex flex-wrap gap-2">
+                    {movieData?.genres?.map((genre, index) => {
+                      return (
+                        <span
+                          key={index}
+                          className="bg-white/5 p-2 rounded text-xs"
+                        >
+                          {genre?.name}
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className="moviePopilarity">
+                  <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400">
+                    Popularity
+                  </p>
+                  <div className="moviePopilarityList flex flex-wrap gap-2">
+                    <span className="bg-white/5 p-2 rounded text-xs">
+                      {movieData?.popularity}
+                    </span>
+                  </div>
+                </div>
+                <div className="movieRevenue">
+                  <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400">
+                    Revenue
+                  </p>
+                  <div className="movieRevenueList flex flex-wrap gap-2">
+                    <span className="bg-white/5 p-2 rounded text-xs">
+                      $
+                      {movieData?.revenue.toLocaleString("en-US", {
+                        formatMatcher: "basic",
+                      })}
+                    </span>
+                  </div>
                 </div>
               </div>
-              <div className="moviePopilarity">
-                <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400 shadow-sm shadow-black rounded">
-                  Popularity
-                </p>
-                <div className="moviePopilarityList flex flex-wrap gap-2">
-                  <span className="bg-white/5 p-2 rounded text-xs">
-                    {movieData?.popularity}
-                  </span>
-                </div>
-              </div>
+
               <div className="movieProductionCompanies">
-                <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400 shadow-sm shadow-black rounded">
+                <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400">
                   Production Companies
                 </p>
                 <div className="movieProductionCompaniesList flex flex-wrap gap-2">
@@ -177,16 +198,6 @@ export const MovieDetails = () => {
                       );
                     })}
                   </ul>
-                </div>
-              </div>
-              <div className="movieRevenue">
-                <p className="text-xs p-2 my-2 uppercase border-l border-red-500 text-gray-400 shadow-sm shadow-black rounded">
-                  Revenue
-                </p>
-                <div className="movieRevenueList flex flex-wrap gap-2">
-                  <span className="bg-white/5 p-2 rounded text-xs">
-                    ${movieData?.revenue.toLocaleString("en-US", {formatMatcher: "basic"})}
-                  </span>
                 </div>
               </div>
             </div>
